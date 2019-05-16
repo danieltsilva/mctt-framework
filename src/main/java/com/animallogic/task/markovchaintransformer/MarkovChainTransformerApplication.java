bceb002ac21bab9c5c288d2d7229ae000592c84d
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-05-16 19:27:04 +1000

    Raw Spring-Boot project

---
+++ src/main/java/com/animallogic/task/markovchaintransformer/MarkovChainTransformerApplication.java
@@ -0,0 +1,13 @@
+package com.animallogic.task.markovchaintransformer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarkovChainTransformerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarkovChainTransformerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/animallogic/task/markovchaintransformer/MarkovChainTransformerApplicationTests.java
@@ -0,0 +1,16 @@
+package com.animallogic.task.markovchaintransformer;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MarkovChainTransformerApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}

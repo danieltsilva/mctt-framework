80e363c34e9cbb7a75c9ea309bf7b4e80e0d22a8
danieltsilva <eugostodequeijadinhadequeijo@gmail.com>
2019-05-17 22:32:24 +1000

    Changing stream for parallelStream when generating text for better performance

--- src/main/java/com/animallogic/markovchaintransformer/services/impl/MarkovChainServiceImpl.java
+++ src/main/java/com/animallogic/markovchaintransformer/services/impl/MarkovChainServiceImpl.java
@@ -30,24 +30,29 @@ public class MarkovChainServiceImpl implements MarkovChainService {
     }
 
     private Map<String, List<String>> createTransitionMatrix(String[] words, int order) {
+        log.debug("SERVICE: createTransitionMatrix countOfWords:{} order:{}", words.length, order);
 
         Map<String, List<String>> transitionMatrix = new HashMap<>();
 
-        //TODO improve loops and try to use Streams
+        // Iterating through all words
         for (int i = 0; i < (words.length - order); ++i) {
-            StringBuilder key = new StringBuilder(words[i]);
+
+            // Creating prefixes according param order
+            StringBuilder prefix = new StringBuilder(words[i]);
             for (int j = i + 1; j < i + order; ++j) {
-                key.append(' ').append(words[j]);
+                prefix.append(' ').append(words[j]);
             }
+
+            // Getting prefix's suffix
             String value = (i + order < words.length) ? words[i + order] : "";
-            if (!transitionMatrix.containsKey(key.toString())) {
-                ArrayList<String> list = new ArrayList<>();
-                list.add(value);
-                transitionMatrix.put(key.toString(), list);
-            } else {
-                transitionMatrix.get(key.toString()).add(value);
-            }
+
+            // Map new tuple ( prefix -> suffix )
+            transitionMatrix
+                    .computeIfAbsent( prefix.toString(),  k -> new ArrayList<>())
+                    .add(value);
+
         }
+        log.debug("Transition Matrix created. Number of prefixes mapped: {}", transitionMatrix.size());
 
         return transitionMatrix;
     }
@@ -65,42 +70,28 @@ public class MarkovChainServiceImpl implements MarkovChainService {
             log.error("outputSize: {}, keySize: {}, wordsLenght: {}", outputSize, keySize, words.length);
             throw new IllegalArgumentException("Output size is out of range");
         }
-        Map<String, List<String>> dict = new HashMap<>();
 
-        //TODO decouple transitional matrix logic
-        for (int i = 0; i < (words.length - keySize); ++i) {
-            StringBuilder key = new StringBuilder(words[i]);
-            for (int j = i + 1; j < i + keySize; ++j) {
-                key.append(' ').append(words[j]);
-            }
-            String value = (i + keySize < words.length) ? words[i + keySize] : "";
-            if (!dict.containsKey(key.toString())) {
-                ArrayList<String> list = new ArrayList<>();
-                list.add(value);
-                dict.put(key.toString(), list);
-            } else {
-                dict.get(key.toString()).add(value);
-            }
-        }
+        // Creating Transitional Matrix
+        Map<String, List<String>> transMatrix = createTransitionMatrix(words, keySize);
 
         int n = 0;
-        int rn = r.nextInt(dict.size());
-        String prefix = (String) dict.keySet().toArray()[rn];
+        int rn = r.nextInt(transMatrix.size());
+        String prefix = (String) transMatrix.keySet().toArray()[rn];
         List<String> output = new ArrayList<>(Arrays.asList(prefix.split(" ")));
 
         //TODO decouple text generator logic
         while (true) {
-            List<String> suffix = dict.get(prefix);
+            List<String> suffix = transMatrix.get(prefix);
             if (suffix.size() == 1) {
-                if (Objects.equals(suffix.get(0), "")) return output.stream().reduce("", (a, b) -> a + " " + b);
+                if (Objects.equals(suffix.get(0), "")) return output.parallelStream().reduce("", (a, b) -> a + " " + b);
                 output.add(suffix.get(0));
             } else {
                 rn = r.nextInt(suffix.size());
                 output.add(suffix.get(rn));
             }
-            if (output.size() >= outputSize) return output.stream().limit(outputSize).reduce("", (a, b) -> a + " " + b);
+            if (output.size() >= outputSize) return output.parallelStream().limit(outputSize).reduce("", (a, b) -> a + " " + b);
             n++;
-            prefix = output.stream().skip(n).limit(keySize).reduce("", (a, b) -> a + " " + b).trim();
+            prefix = output.parallelStream().skip(n).limit(keySize).reduce("", (a, b) -> a + " " + b).trim();
         }
     }
 
